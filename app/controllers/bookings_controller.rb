9424af13428e8a7888935d09d0967136ccd31e6e
Thomas Ng <D3icidal@gmail.com>
2018-12-02 17:19:33 -0500

    add booking create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,20 @@
 class BookingsController < ApplicationController
+
   def create #Host creates a new booking
-    if current_user
-      puts "\n\n\tCurrent User: #{current_user} #{current_user.email}\t*******\n\n" 
-    else
-      puts "\n\n\t No User Logged in !!! ********* \n\n"
-      puts "\n\n\tfailed in index**********\n"
-      render json: {}, status: :bad_request
-    end
+    # if current_user
+    #   puts "\n\n\tCurrent User: #{current_user} #{current_user.email}\t*******\n\n" 
+    # else
+    #   puts "\n\n\t No User Logged in !!! ********* \n\n"
+    #   puts "\n\n\tfailed in create for bookings controller**********\n"
+    #   render json: {}, status: :bad_request
+    # end
 
     booking = Booking.new(
-      home_id: Home.find_by(id: params[:homes_id]),
-      checkout_reason: params[:checkout_reason], #should be nil unless booking was forced by host for testing.
-      code: "EXXSTEST" #TODO fake code for testing, should implement faker or hex-gen
+      home_id: params[:home_id], #TODO redudant search,
+      checkin: params[:checkin],
+      checkout: params[:checkout],
+      checkout_reason: params[:checkout_reason] || nil, #should be nil unless booking was forced by host for testing.
+      code: params[:code] || "EXXSTEST" #TODO fake code for testing, should implement faker or hex-gen
       )
 
     if booking.save
@@ -19,6 +22,7 @@ class BookingsController < ApplicationController
       render json: {message: 'booking created successfully'}, status: :created
     else
       puts "\n\n\tinside create, booking failed! #{booking}\n\n"
+       booking.save!
       render json: {errors: booking.errors.full_messages}, status: :bad_request
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,12 @@ Rails.application.routes.draw do
   get '/homes/:id' => 'homes#show'      #Host sees one specific home
   get 'homes/:id/edit' => 'homes#edit'  #Host goes to a page to submit edits for a home's details
   patch '/homes/:id' => 'homes#update'  #Home - changes get commited to DB
-  delete 'homes/:id' => 'homes#destroy' #Home gets marked as inactive in DB
+  delete '/homes/:id' => 'homes#destroy' #Home gets marked as inactive in DB
 
+  #BOOKINGS (stays)
+  post '/bookings' => 'bookings#create'     #host creates new booking
+
+  
 
   #BOOKING-SESSIONS (GUEST LOGINS)
   #get '/bookings/sessions/new' => 'booking_sessions#new'     #guestlogin to see home - handled in view
